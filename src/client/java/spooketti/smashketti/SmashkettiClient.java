8417cb1635b17a8e16d9a1f4adaa475dd938a29b
spooketti <jonliu168@gmail.com>
2024-08-08 00:58:43 -0700

    where is this going?

---
+++ src/client/java/spooketti/smashketti/SmashkettiClient.java
@@ -0,0 +1,10 @@
+package spooketti.smashketti;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmashkettiClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
