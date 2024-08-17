d666699e326c30aaa9099f9e1449de4d8236e362
spooketti <jonliu168@gmail.com>
2024-08-17 16:32:22 -0700

    gonna go swimming leaving my jsons / textures here

--- src/client/java/spooketti/smashketti/SmashkettiClient.java
+++ src/client/java/spooketti/smashketti/SmashkettiClient.java
@@ -3,6 +3,7 @@ package spooketti.smashketti;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -24,11 +25,11 @@ public class SmashkettiClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		ClientTickEvents.END_CLIENT_TICK.register(ClientSmashAttack::smashAttackLogic);
 		ClientTickEvents.END_CLIENT_TICK.register(ClientGrab::grabLogic);
 //		ClientTickEvents.END_CLIENT_TICK.register(SideCamera::cameraLogic);
 	}
+
+
 
 }
